c1ae85fe2632582b8edb8124112b8943b78515b3
Kelly Plageman <KPlageman@prokarma.com>
2019-08-05 22:33:43 -0500

    Added Pages for rerouting after auth is completed

---
+++ src/app/ranks/ranks.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ranks',
+  templateUrl: './ranks.page.html',
+  styleUrls: ['./ranks.page.scss'],
+})
+export class RanksPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
