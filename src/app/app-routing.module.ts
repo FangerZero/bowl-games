8e198f315d800dc347147530830cbe23c0022d55
Kelly Plageman <KPlageman@prokarma.com>
2019-08-27 10:55:36 -0500

    Add Admin Bowl, Game, Team New and Edit pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './pages/auth/auth.guard';
 import { AdminGuard } from './pages/admin/admin.guard';
 
 const routes: Routes = [
+  // Main Site
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   { path: 'auth', loadChildren: './pages/auth/auth.module#AuthPageModule' },
@@ -13,10 +14,17 @@ const routes: Routes = [
   { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule', canLoad: [AuthGuard] },
   { path: 'selections', loadChildren: './pages/selections/selections.module#SelectionsPageModule', canLoad: [AuthGuard] },
   { path: 'support', loadChildren: './pages/support/support.module#SupportPageModule' },
+  // Admin
+  { path: 'admin/games/new', loadChildren: './pages/admin/games/new/new.module#NewPageModule', canLoad: [AuthGuard, AdminGuard] },
+  { path: 'admin/games/edit', loadChildren: './pages/admin/games/edit/edit.module#EditPageModule', canLoad: [AuthGuard, AdminGuard] },
   { path: 'admin/games', loadChildren: './pages/admin/games/games.module#GamesPageModule', canLoad: [AuthGuard, AdminGuard] },
+  { path: 'admin/teams/new', loadChildren: './pages/admin/teams/new/new.module#NewPageModule', canLoad: [AuthGuard, AdminGuard] },
+  { path: 'admin/teams/edit', loadChildren: './pages/admin/teams/edit/edit.module#EditPageModule', canLoad: [AuthGuard, AdminGuard] },
   { path: 'admin/teams', loadChildren: './pages/admin/teams/teams.module#TeamsPageModule', canLoad: [AuthGuard, AdminGuard] },
+  { path: 'admin/bowls/new', loadChildren: './pages/admin/bowls/new/new.module#NewPageModule', canLoad: [AuthGuard, AdminGuard] },
+  { path: 'admin/bowls/edit', loadChildren: './pages/admin/bowls/edit/edit.module#EditPageModule', canLoad: [AuthGuard, AdminGuard] },
   { path: 'admin/bowls', loadChildren: './pages/admin/bowls/bowls.module#BowlsPageModule', canLoad: [AuthGuard, AdminGuard] },
-  { path: 'admin', loadChildren: './pages/admin/admin.module#AdminPageModule', canLoad: [AuthGuard, AdminGuard] },
+  // { path: 'admin', loadChildren: './pages/admin/admin.module#AdminPageModule', canLoad: [AuthGuard, AdminGuard] },
 ];
 
 @NgModule({
