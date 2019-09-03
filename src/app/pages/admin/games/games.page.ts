6e87fe77c2066cd100eb4c7cf2d8c91b400f3107
Kelly Plageman <KPlageman@prokarma.com>
2019-09-03 10:50:25 -0500

    Add bowl and team names to Games pages

--- src/app/pages/admin/games/games.page.ts
+++ src/app/pages/admin/games/games.page.ts
@@ -20,7 +20,12 @@ export class GamesPage implements OnInit {
   loadedTeams: Team[];
   loadedBowls: Bowl[];
 
-  constructor(private gamesService: GamesService, private teamsService: TeamsService, private bowlsService: BowlsService, private router: Router) { }
+  constructor(
+    private gamesService: GamesService,
+    private teamsService: TeamsService,
+    private bowlsService: BowlsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.gamesService.games.subscribe(games => {
@@ -50,5 +55,17 @@ export class GamesPage implements OnInit {
 
   onEditGame(gameId: number) {
     this.router.navigateByUrl(`admin/games/edit/${gameId}`);
+  }
+
+  getBowlName(bowlId: number) {
+    if (this.loadedBowls) {
+      return this.loadedBowls.find(element => element.id === bowlId).name;
+    }
+  }
+
+  getTeamName(teamId: number) {
+    if (this.loadedTeams) {
+      return this.loadedTeams.find(element => element.id === teamId).name;
+    }
   }
 }
